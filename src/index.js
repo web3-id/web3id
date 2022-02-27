a6b34815ed1dc79ce726e3a4e6939ee74a01aecf
abraham-yusuf <1703020028@students.unis.ac.id>
2022-02-27 17:41:49 +0000

    added animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./assets/animated.css";
+import './assets/animated.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import '../node_modules/elegant-icons/style.css';
 import '../node_modules/et-line/style.css';
